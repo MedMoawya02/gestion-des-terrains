990d75e5866a220efb349d3360c7e21d8c194a75
MedMoawya02 <mohammed.mouawya2002@gmail.com>
2026-02-06 11:18:26 +0100

    feat:L'ajout de role dans la table users et l'ajout du table terrain

---
+++ database/migrations/2026_02_06_100907_create_terrains_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('terrains', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('adresse');
+            $table->enum('type', ['7vs7','11vs11']);
+            $table->float('prix_par_heure');
+            $table->enum('statut', ['disponible', 'En maintenance'])->default('disponible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('terrains');
+    }
+};
